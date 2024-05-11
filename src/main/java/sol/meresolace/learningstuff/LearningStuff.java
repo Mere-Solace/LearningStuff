5b42981a474d255a12c23e664f40e97eb4c48661
Mere Solace <btm7428@gmail.com>
2024-05-10 20:31:50 -0400

    Join Event

--- src/main/java/sol/meresolace/learningstuff/LearningStuff.java
+++ src/main/java/sol/meresolace/learningstuff/LearningStuff.java
@@ -1,17 +1,19 @@
 package sol.meresolace.learningstuff;
 
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.player.PlayerJoinEvent;
 import org.bukkit.plugin.java.JavaPlugin;
 
-public final class LearningStuff extends JavaPlugin {
+public final class LearningStuff extends JavaPlugin implements Listener{
 
     @Override
-    public void onEnable() {
-        // Plugin startup logic
-
+    public void onEnable(){
+        System.out.println("The plugin has started.");
     }
 
-    @Override
-    public void onDisable() {
-        // Plugin shutdown logic
+    @EventHandler
+    public void onPlayerJoin(PlayerJoinEvent event){
+        System.out.println("Young son thinks he's hip with a name like \"" + event.getPlayer().getDisplayName() + "\"");
     }
 }
